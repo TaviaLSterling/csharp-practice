9a4fd7093f146bb2b95690b7e59fec009fd990ec
Tavia Sterling <TaviaSterling@outlook.com>
2018-11-06 12:25:33 -0700

    string expressions

---
+++ BeginToCodeWithCSharp/My Snaps apps/SpeakTheDay.cs
@@ -0,0 +1,14 @@
+﻿using SnapsLibrary;
+
+public class SpeakTheDay
+{
+    public void StartProgram()
+    {
+        string myName = SnapsEngine.ReadString("What is your name?");
+        string dayName = SnapsEngine.GetDayOfWeekName();
+        SnapsEngine.DisplayString("Good day" + myName + "How are you doing on this fine " + dayName);
+        SnapsEngine.SpeakString("Good day " +  myName  + " . How are you doing on this fine " + dayName + " ?");
+    }
+}
+
+
