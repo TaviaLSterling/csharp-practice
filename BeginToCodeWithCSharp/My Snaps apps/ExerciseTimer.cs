acbff29e2356e07071acefd3836f39737c7e8683
Tavia Sterling <TaviaSterling@outlook.com>
2018-11-06 11:50:19 -0700

    exercise timer

---
+++ BeginToCodeWithCSharp/My Snaps apps/ExerciseTimer.cs
@@ -0,0 +1,36 @@
+﻿using SnapsLibrary;
+
+public class ExerciseTimer
+{
+    public void StartProgram()
+    {
+        SnapsEngine.SetTitleString("Workout Time!");
+        SnapsEngine.DisplayString("Jog in Place");
+        SnapsEngine.SpeakString("start Jogging in Place");
+        SnapsEngine.Delay(30);
+        SnapsEngine.SpeakString("rest");
+        SnapsEngine.Delay(10);
+        SnapsEngine.DisplayString("Push-ups");
+        SnapsEngine.SpeakString("start push ups");
+        SnapsEngine.Delay(30);
+        SnapsEngine.SpeakString("rest");
+        SnapsEngine.Delay(10);
+        SnapsEngine.DisplayString("Jumping Jacks");
+        SnapsEngine.SpeakString("start Jumping jacks");
+        SnapsEngine.Delay(30);
+        SnapsEngine.SpeakString("rest");
+        SnapsEngine.Delay(10);
+        SnapsEngine.DisplayString("Squats");
+        SnapsEngine.SpeakString("start squats");
+        SnapsEngine.Delay(30);
+        SnapsEngine.SpeakString("rest");
+        SnapsEngine.Delay(10);
+        SnapsEngine.DisplayString("Sit-ups");
+        SnapsEngine.SpeakString("start sit ups");
+        SnapsEngine.Delay(30);
+        SnapsEngine.SpeakString("rest");
+        SnapsEngine.Delay(10);
+    }
+}
+
+
