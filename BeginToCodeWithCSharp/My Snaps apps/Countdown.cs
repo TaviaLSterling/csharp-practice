d38cdb351fbe9e942f98ba7f1838e75e847c2b34
Tavia Sterling <TaviaSterling@outlook.com>
2018-11-06 11:31:41 -0700

    Countdown

---
+++ BeginToCodeWithCSharp/My Snaps apps/Countdown.cs
@@ -0,0 +1,34 @@
+﻿using SnapsLibrary;
+
+public class Countdown
+{
+    public void StartProgram()
+    {
+        SnapsEngine.SetTitleString("Countdown");
+        SnapsEngine.DisplayString("10");
+        SnapsEngine.SpeakString("ten");
+        SnapsEngine.DisplayString("9");
+        SnapsEngine.SpeakString("nine");
+        SnapsEngine.DisplayString("8");
+        SnapsEngine.SpeakString("eight");
+        SnapsEngine.DisplayString("7");
+        SnapsEngine.SpeakString("seven");
+        SnapsEngine.DisplayString("6");
+        SnapsEngine.SpeakString("six");
+        SnapsEngine.DisplayString("5");
+        SnapsEngine.SpeakString("five");
+        SnapsEngine.DisplayString("4");
+        SnapsEngine.SpeakString("four");
+        SnapsEngine.DisplayString("3");
+        SnapsEngine.SpeakString("three");
+        SnapsEngine.DisplayString("2");
+        SnapsEngine.SpeakString("two");
+        SnapsEngine.DisplayString("1");
+        SnapsEngine.SpeakString("one");
+        SnapsEngine.DisplayString("Blast off!");
+        SnapsEngine.SpeakString("blast off");
+
+    }
+}
+
+
