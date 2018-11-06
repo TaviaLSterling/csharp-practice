44e3c8fb0557206e14e159a54806e845fb34652e
Tavia Sterling <TaviaSterling@outlook.com>
2018-11-06 10:26:57 -0700

    Add project files.

---
+++ BeginToCodeWithCSharp/My Snaps apps/MyProgram.cs
@@ -0,0 +1,12 @@
+﻿using SnapsLibrary;
+
+public class MyProgram
+{
+    public void StartProgram()
+    {
+        SnapsEngine.SetTitleString("Tavia Can Code with C#");
+        SnapsEngine.DisplayString("Welcome to the world of Snaps");
+    }
+}
+
+
