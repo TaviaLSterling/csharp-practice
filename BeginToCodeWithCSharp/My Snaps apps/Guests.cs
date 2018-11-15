cca92f9c69336e34d8e4e41c92de9f366901e783
Tavia Sterling <TaviaSterling@outlook.com>
2018-11-15 14:22:21 -0700

    Guest List

---
+++ BeginToCodeWithCSharp/My Snaps apps/Guests.cs
@@ -0,0 +1,31 @@
+﻿using SnapsLibrary;
+
+class Guests
+{
+    public void StartProgram()
+    {
+        SnapsEngine.SetTitleString("Guest List");
+
+        // Find out how many sales values are being stored
+        int noOfGuests = SnapsEngine.ReadInteger("How many guests will there be?");
+        int[] guests = new int[noOfGuests];
+
+        // Loop round and read the sales values
+        for (int count = 0; count < guests.Length; count = count + 1)
+        {
+            // User likes to count from 1, not zero
+            int displayCount = count + 1;
+            guests[count] = SnapsEngine.ReadString("Enter the name for guest " + displayCount);
+        }
+
+        // Got the sales figures, now display them
+
+        SnapsEngine.ClearTextDisplay();
+
+        // Add a line to the display for each sales figure
+        for (int count = 0; count < guests.Length; count = count + 1)
+        {
+            SnapsEngine.AddLineToTextDisplay("Name: " + guests[count]);
+        }
+    }
+}
